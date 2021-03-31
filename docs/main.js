fb1ecee70b6e544845d3c1b61a6ec45af2583f9c
Emerson Filho <emerson.dev.machado@gmail.com>
2021-03-31 04:06:25 -0400

    function of number, dot and clear ready

--- docs/main.js
+++ docs/main.js
@@ -1,131 +1,79 @@
-//variable to receive the client click
-let clientClick = []
-//variable to receive all calc
-let calcPreview = []
+let htmlInput = document.querySelector('#visorInferior')
+let htmlOutput = document.querySelector('#visorSuperior')
+htmlInput.focus()
+
+//is a variable to receive the number clicked on the html
+let clientInput = ''
+let fullCalc = []
 
 
-
-let index = 0
-let num1 = 0
-let firstNumber = ''
-let secondNumber = ''
-let operator = ''
-let lastResult = ''
-
-
-
-//mostrar todo o calculo no visor superior
-const allCalc = document.querySelector('#visorSuperior')
-const lastInput = document.querySelector('#visorInferior')
-
-
-//teste variables in console
-const variablesTeste = () => {
-    console.log('clientClick = ',clientClick)
-    console.log('calcPreview = ',calcPreview)
-    console.log('firstNumber = ',firstNumber)
-    console.log('secondNumber = ',secondNumber)
-    console.log('operator = ',operator)
-    allCalc.innerText = calcPreview.join('')
-    lastInput.value = clientClick.join('')
-    console.log('-------------------')
+//is a function to show all in html
+const showAll = () => {
+    htmlInput.value = clientInput
+    htmlOutput.innerText = fullCalc.join('')
+    console.log('clientInput = ', clientInput)
+    console.log('fullCalc = ', fullCalc)
 }
 
-//functions of operator
-const answer = () => {
-    if (operator === '+') {
-        return firstNumber + secondNumber
 
-    } else if (operator === '-') {
-        return firstNumber - secondNumber
-
-    } else if (operator === '*') {
-        return firstNumber * secondNumber
-
-    } else if (operator === '/') {
-        return firstNumber / secondNumber
-    }   
-}
-
-//function to add the number clicked in variable clientClick
-function numberClick (numberSelected) {
-    clientClick.push(Number(numberSelected.value))
-    variablesTeste()
-}
-
-//function to add a dot when button pressed
-function dotClick (dot) {
-    if (clientClick[clientClick.length - 1] != '.') {
-        clientClick.push(dot.value)
+//is a function to add the number clicked
+function numberClick (numberCliked) {
+    if (numberCliked.value == 0 && clientInput == '') {} else {
+        clientInput += numberCliked.value
     }
-    variablesTeste()
+    showAll()    
 }
 
-//function to add the operator clicked and all number in variable calcPreview
-function operatorClick (operatorSelected) {
-    operator = operatorSelected.value 
-    calcPreview.push(...clientClick, operator)
-    if (lastResult != '') {
-        firstNumber = lastResult
-        secondNumber = Number(clientClick.join(''))
-        let result = answer()
-        lastResult = result
-    } else if (firstNumber === '') {
-        firstNumber = Number(clientClick.join(''))
+
+//is a function to add the dot
+function dotClick (dotClicked, findDot) {
+    findDot = false
+    for (let index = 0; index < htmlInput.value.length; index++) {
+        if (htmlInput.value.charAt(index) === dotClicked.value) {
+            findDot = true
+        }
+    }
+    if (clientInput == '') {
+        clientInput += '0' + dotClicked.value
+    } else if (findDot == false) {
+        clientInput += dotClicked.value
+    }
+    showAll()
+}
+
+
+//is a function to clear the last number clicked
+function clearVariables (optionSelected) {
+    if (optionSelected.value === 'C') {
+        clientInput = clientInput.replace(clientInput[clientInput.length -1], '')
+    } else if (optionSelected.value === 'AC') {
+        clientInput = ''
+    }
+    showAll()
+}
+
+
+//is a function to take the operator and return the result
+function operatorClick (operatorClicked) {
+    if (clientInput != '') {
+        if (clientInput[clientInput.length -1] == '.') { 
+            fullCalc.push(clientInput, '0')
+        }
+    }
+    if (!isNaN(fullCalc[fullCalc.length -1])) {//ultimo elemento ser numero
+        fullCalc.push(operatorClicked.value)
     } else {
-        secondNumber = Number(clientClick.join(''))
-        let result = answer()
-        lastResult = result
+        fullCalc.pop()
+        fullCalc.push(operatorClicked.value)
     }
-    clientClick = []
-    console.log('lastResult = ', lastResult)
-    console.log('firstNumber = ',firstNumber)
-    console.log('secondNumber = ',secondNumber)
-    console.log('operator = ',operator)
-}
+    clientInput = ''
+    
 
-//function to options C and AC
-function clearVariables (clearOption) {
-    if (clearOption.value == 'C') {
-        clientClick = []
-        variablesTeste()
-    } else {
-        clientClick = []
-        calcPreview = []
-        variablesTeste()
-    }
-}
-
-//function to show the result
-function equals () {
-    if (lastResult != '') {
-        firstNumber = lastResult
-        secondNumber = Number(clientClick.join(''))
-        let result = answer()
-        lastResult = result
-    } else if (firstNumber === '') {
-        firstNumber = Number(clientClick.join(''))
-    } else {
-        secondNumber = Number(clientClick.join(''))
-        let result = answer()
-        lastResult = result
-    }
-
-
-
-
-
-    console.log('resultado final = ', lastResult)
-}
-
-//taking the key press on keyboard and add function
-document.addEventListener('keypress', function (click) {
-
-    // if (click.key >= 0) {
-    //     numberClick(click.key)
-    // } else if (click.key == '.' || click.key == ',') {
-    //     dotClick(click.key)
-    // } else if (click.key == '+' || click.key == '-' || click.key == '*' || click.key == '/') {
-    //     operatorClick(click.key)
+    // for (let index = 0; index < fullCalc.length; index++) {
+    //     if (fullCalc[index] == isNaN){
+    //         num1 = fullCalc[index]
+    //         console.log('num1 = ', num1)
+    //     }
     // }
-})
+    showAll()
+}
--- docs/script.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('#visorInferior').focus()
-
-let visorInferior = document.querySelector('#visorInferior')
-let visorSuperior = document.querySelector('#visorSuperior')
-
-
